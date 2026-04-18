0e4573d8225d54536d0a0ff17c56a045bc6ff5ce
Sabina <sabinameche22@gmail.com>
2026-04-18 17:08:13 +0545

    access token and refresh token

--- backend/roomease/api/views_dir/register_view.py
+++ backend/roomease/api/views_dir/register_view.py
@@ -8,7 +8,7 @@ from rest_framework import status
 class RegisterView(APIView):
     def post(self,request):
         data = request.data
-        print("printing data",data)
+       
         serializer = RegisterSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
@@ -18,17 +18,18 @@ class RegisterView(APIView):
 
 class LoginView(APIView):
     def post(self,request):
+        print("am i here though")
         serializer = LoginSerializer(data=request.data)
         if serializer.is_valid():
             username = serializer.validated_data['username']
             password = serializer.validated_data['password']
             user = authenticate(username=username,password=password)
 
-        if user is not None:
-            refresh = RefreshToken.for_user(user)
-            return Response({
-                'refresh_token':str(refresh),
-                'access_token':str(refresh.access_token)
+            if user is not None:
+                refresh = RefreshToken.for_user(user)
+                return Response({
+                    'refresh_token':str(refresh),
+                    'access_token':str(refresh.access_token)
             })
         return Response({'success':False,
                          "message":"Invalid Credentials"},status=401)
