0dd32584b2c3d420451225bfdffd436cf0d4523a
Sabina <sabinameche22@gmail.com>
2026-09-09 22:13:33 +0545

    fixed the user who paid without expenseSplit

--- backend/roomease/api/services/expense_owned_services.py
+++ backend/roomease/api/services/expense_owned_services.py
@@ -15,23 +15,27 @@ class ExpenseOwnedService:
         # amount they owes per user after split
         amount_per_user = ExpenseSplit.objects.filter(expense__group = groupId).values('user__id').annotate(total_owes =Sum('amount'))
 
-        print('amount per user ma kati kasare k aauxa',amount_per_user)
         # amount per user paid
         
         paid_per_user = Expense.objects.filter(group = groupId).values('paid_by').annotate(total_paid = Sum('amount'))
-        
+
+        balance_per_user = {}
+        for user in amount_per_user:
+            balance_per_user[user['user__id']] = user['total_owes']
+
+        creditor = {}
+        debtor = {}
+            
         paid_by_user = {}
         for per_user in paid_per_user:
-            print(per_user['paid_by'])
+           
 
-            if per_user['paid_by'] not in amount_per_user['user__id']:
+            if per_user['paid_by'] not in balance_per_user:
                 creditor[per_user['paid_by']] = per_user['total_paid']
                 
             paid_by_user[per_user['paid_by']] = per_user['total_paid']
         
-        creditor = {}
-        debtor = {}
-        print('la paid by user ma ni k xa herxu',paid_by_user)
+    
         for user in amount_per_user:
 
             if user['user__id'] in paid_by_user:
